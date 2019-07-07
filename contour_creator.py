30051e5d6888ecd58a507182253815f5112fb3b4
Drew Ellingson <drew.ellingson@gmail.com>
2019-07-06 17:35:57 -0700

    add docstrings and formatted code

--- contour_creator.py
+++ contour_creator.py
@@ -1,17 +1,19 @@
 import numpy as np
 from skimage import io, viewer, exposure
-from skimage.transform import rescale, resize, downscale_local_mean
-import matplotlib
+from skimage.transform import resize
 import easygui
 import math
 
+
 def convolution_zoom_in(img_matrix, mask_matrix):
+    """ convolution calculation at one matrix entry given a mask"""
     flat_img = img_matrix.flatten()
     flat_mask = mask_matrix.flatten()
     return sum([a*b for a, b in zip(flat_img, flat_mask)])
 
 
 def convolution(input_img, mask_matrix):
+    """convolution of any sized image given a mask matrix"""
     x_size, y_size = input_img.shape
     output_img = np.empty(input_img.shape)
     for x_pix in range(0, x_size):         # handling image corners and edges
@@ -34,6 +36,7 @@ def convolution(input_img, mask_matrix):
 
 
 def produce_x_grad(input_img):
+    """ generate x derivative of given image"""
     x_diff = np.array([[-1, 0, 1],
                        [-2, 0, 2],
                        [-1, 0, 1]])
@@ -41,6 +44,7 @@ def produce_x_grad(input_img):
 
 
 def produce_y_grad(input_img):
+    """generate y derivative of given image"""
     y_diff = np.array([[-1, -2, -1],
                        [0, 0, 0],
                        [1, 2, 1]])
@@ -48,17 +52,18 @@ def produce_y_grad(input_img):
 
 
 def produce_overall_grad(input_img):
+    """produce sobel derivative of input image"""
     x_grad = produce_x_grad(input_img)
-
     y_grad = produce_y_grad(input_img)
-
 
     return (x_grad**2+y_grad**2)**0.5
 
+
 def main():
+    """pick an image, resize for easy computation, display original vs contour"""
     my_file = easygui.fileopenbox()
 
-    assert my_file.endswith('.png') or my_file.endswith('.jpg'),'please select a .jpg or .png image file'
+    assert my_file.endswith('.png') or my_file.endswith('.jpg'), 'please select a .jpg or .png image file'
 
     my_img = io.imread(my_file, as_gray=True)
 
@@ -71,18 +76,17 @@ def main():
         (math.floor(x_size * scaling_factor), math.floor(y_size * scaling_factor)),
         anti_aliasing=True)
 
-    x_grad = exposure.equalize_hist(produce_x_grad(my_img))
-    y_grad = exposure.equalize_hist(produce_y_grad(my_img))
     overall_grad_img = exposure.equalize_hist(produce_overall_grad(my_img))
 
     output_img = np.empty(my_img.shape)
 
     for column in range(0, len(overall_grad_img)):
         for value in range(0, len(overall_grad_img[column])):
-            output_img[column,value] = 1-overall_grad_img[column,value]
+            output_img[column, value] = 1-overall_grad_img[column, value]
 
     final_output = np.concatenate([my_img, output_img], axis=1)
     viewer.ImageViewer(final_output).show()
+
 
 if __name__ == '__main__':
     main()
